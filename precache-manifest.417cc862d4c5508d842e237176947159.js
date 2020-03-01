1fb18035ca3cdd0d88d2a9cf78829e34665d3aef
Gosia Barlik-Bokowy <malgorzata.barlik@gmail.com>
2020-03-01 11:02:04 +0100

    Updates

---
+++ precache-manifest.417cc862d4c5508d842e237176947159.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94838af64ab91b7f0f1c56a0035f70ef",
+    "url": "/Plantcare/index.html"
+  },
+  {
+    "revision": "72871e449d152ff248e3",
+    "url": "/Plantcare/static/js/2.a5413c5a.chunk.js"
+  },
+  {
+    "revision": "f1d64bda243e7b9be69d1aa25980f07b",
+    "url": "/Plantcare/static/js/2.a5413c5a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "af638f08e4f67980c8f9",
+    "url": "/Plantcare/static/js/main.b791107e.chunk.js"
+  },
+  {
+    "revision": "cda300918b3fb17823da",
+    "url": "/Plantcare/static/js/runtime-main.569301af.js"
+  }
+]);
